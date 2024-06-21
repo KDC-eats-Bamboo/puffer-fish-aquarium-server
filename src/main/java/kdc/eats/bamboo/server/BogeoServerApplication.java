0512b27e9ee66f00527757665ddcdf9c5675e501
이슬아 <rheeseula@gmail.com>
2024-06-21 11:08:18 +0900

    Feat :: Initialized Spring Boot Application

---
+++ src/main/java/kdc/eats/bamboo/server/BogeoServerApplication.java
@@ -0,0 +1,13 @@
+package kdc.eats.bamboo.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BogeoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BogeoServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kdc/eats/bamboo/server/BogeoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kdc.eats.bamboo.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BogeoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
